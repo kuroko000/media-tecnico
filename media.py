6e3306e34130f6b61b3180ef78c47e48275d34e0
kuroko000 <121641551+kuroko000@users.noreply.github.com>
2024-04-25 17:47:29 -0300

    Update media.py

---
+++ media.py
@@ -0,0 +1,18 @@
+#vamos desenvolver um programa que calcula a media entre duas notas
+
+#nesse "input" abaixo vamos explica o que nosso programa vai fazer
+input("Vamos calcular a media entre duas notas, aperte enter para continuar:...\n")
+
+Nota1 = float(input("Fale a primeira nota: ")) #adicionar um dos valores para pode fazer a media
+Nota2 = float(input("Fale a segundanota: ")) #adicionar um outro valor para pode calcular a media
+
+notafinal = Nota1 + Nota2 
+
+if notafinal >= 7.0 :
+    print("Meus parabens, você foi aprovado !")
+
+else:
+    falta = notafinal - 7.0
+    print ("Você foi reprovado, é lhe faltaram ", falta," pontos para poder passar.")
+
+    
